a5cd199911130615e7969b2c5331491bd7522e1d
Gregory Rudolph <rudi@lolay.com>
2015-09-16 10:02:50 -0400

    Initial Commit

---
+++ Slack-erface/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Slack-erface
+//
+//  Created by Gregory Rudolph-Alverson on 9/16/15.
+//  Copyright (c) 2015 STEiN-Net. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Slack-erface/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Slack-erface
+//
+//  Created by Gregory Rudolph-Alverson on 9/16/15.
+//  Copyright (c) 2015 STEiN-Net. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
